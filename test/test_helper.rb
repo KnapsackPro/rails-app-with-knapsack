a269a5b7ab972b9ee47aeb2e607e2ea24da1ecd0
ArturT <arturtrzop@gmail.com>
2015-05-01 13:53:24 +0200

    Add knapsack and bind minitest adapter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,13 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/spec'
 
+require 'knapsack'
+
+# CUSTOM_CONFIG_GOES_HERE
+
+knapsack_adapter = Knapsack::Adapters::MinitestAdapter.bind
+knapsack_adapter.set_test_helper_path(__FILE__)
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
